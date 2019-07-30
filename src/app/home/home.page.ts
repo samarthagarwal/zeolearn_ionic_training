1a096aaa07aee0a09d3c7d09214a87ad10490e78
Samarth Agarwal <samarthagarwal@live.com>
2019-07-30 09:41:42 +0530

    Ionic ngFor and ngIf

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  trainingName: string = "Ionic Framework Training";
+  visibility: boolean = true;
+
+  people: any[] = [{
+    "name": "Samarth",
+    "age": 28
+  }, {
+    "name": "James",
+    "age": 23
+  }, {
+    "name": "Bob",
+    "age": 12
+  }, {
+    "name": "Alice",
+    "age": 32
+  }, {
+    "name": "Travis",
+    "age": 29
+  }];
+  
+  constructor() {
+
+  }
+
+  toggleVisibility() : void {
+    this.visibility = !this.visibility;
+  }
 
 }
